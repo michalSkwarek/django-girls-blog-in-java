03e837ba7d6066fb48f9ddedf7beebf989e1f867
Michal <skwarek.michal.adrian@gmail.com>
2017-01-10 13:20:28 +0100

    Add spring boot app config

---
+++ DjangoGirlsBlogInJava/src/main/java/com/skwarek/blog/configuration/Application.java
@@ -0,0 +1,16 @@
+package com.skwarek.blog.configuration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Michal on 16.09.2016.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Application.class, args);
+    }
+}

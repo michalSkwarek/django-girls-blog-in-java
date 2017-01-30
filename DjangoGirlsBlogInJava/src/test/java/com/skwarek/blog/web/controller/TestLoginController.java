11979ac62f7c5af55703104a83a625285235d2a8
Michal <skwarek.michal.adrian@gmail.com>
2017-01-30 22:57:11 +0100

    Add test for login controller

--- DjangoGirlsBlogInJava/src/test/java/com/skwarek/blog/web/controller/TestLoginController.java
+++ DjangoGirlsBlogInJava/src/test/java/com/skwarek/blog/web/controller/TestLoginController.java
@@ -7,17 +7,14 @@ import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import static org.hamcrest.core.Is.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * Created by Michal on 05/01/2017.
  */
 @RunWith(MockitoJUnitRunner.class)
 public class TestLoginController {
-
-    private static final String VIEWS_LOGIN_FORM = "registration/login";
 
     private LoginController loginController;
 
@@ -32,23 +29,8 @@ public class TestLoginController {
     }
 
     @Test
-    public void initLoginForm_ShouldRenderLoginView() throws Exception {
-        mockMvc.perform(get("/accounts/login"))
-                .andExpect(status().isOk())
-                .andExpect(model().attributeDoesNotExist("error"))
-                .andExpect(forwardedUrl(VIEWS_LOGIN_FORM))
-                .andExpect(view().name(VIEWS_LOGIN_FORM));
-    }
-
-    @Test
-    public void failedUsernameOrPassword_ShouldHasError() throws Exception {
-        mockMvc.perform(get("/accounts/login")
-                .param("error", "true")
-        )
-                .andExpect(status().isOk())
-                .andExpect(model().attributeExists("error"))
-                .andExpect(model().attribute("error", is(true)))
-                .andExpect(forwardedUrl(VIEWS_LOGIN_FORM))
-                .andExpect(view().name(VIEWS_LOGIN_FORM));
+    public void loginController_ShouldSendUserData() throws Exception {
+        mockMvc.perform(get("/user"))
+                .andExpect(status().isOk());
     }
 }
